b93d9be84ba02f645a6d5a9a6841b7d8be548f8e
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-04-15 17:20:19 +0700

    cleam up

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,119 @@ const textRegex = /^g\/[0-9]{6}$/m;
 const app = express();
 // app.use(express.json());
 
+app.post("/callback", middleware(middlewareConfig), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+async function handleEvent(event: WebhookEvent) {
+  try {
+    if (event.type !== "message" || event.message.type !== "text") {
+      // ignore non-text-message event
+      return Promise.resolve(null);
+    }
+    const text: string = event.message.text;
+    /* Quit */
+    if (text === "/quit") {
+      switch (event.source.type) {
+        case "room":
+          const roomId: string = event.source.roomId;
+          return client.leaveRoom(roomId);
+        case "group":
+          const groupId: string = event.source.groupId;
+          return client.leaveGroup(groupId);
+
+        default:
+          return Promise.resolve(null);
+      }
+
+      /* If message is g/xxxxxx */
+    } else if (textRegex.test(text)) {
+      const { data } = await axios.get(`https://nhentai.net/${text}/`);
+      const $ = cheerio.load(data);
+      /* GETTING TITLE */
+      const title: string = $("h1[class = title]").text();
+
+      /* GETTING TAGS*/
+      let tagResult = $('section[id="tags"]').find(
+        `div:contains("Tags") > span[class=tags] > a > span[class=name]`
+      );
+      const tagsLength: number = tagResult.length;
+      let tags: string[] = [];
+      if (tagsLength > 5) {
+        tagResult = tagResult.slice(0, 5);
+      }
+      await Promise.all(
+        tagResult.map((el) => {
+          tags.push(` ${$(el).text()}`);
+        })
+      );
+      if (tagsLength > 5) {
+        tags.push(" ...");
+      }
+
+      /* GETTING IMAGES*/
+      let imageResult = $("body").find(
+        "#content > #thumbnail-container > .thumbs > .thumb-container"
+      );
+      const pageLength: number = imageResult.length;
+      let images: TemplateImageColumn[] = [];
+      if (imageResult.length > 5) {
+        imageResult = imageResult.slice(0, 5);
+      }
+      await Promise.all(
+        imageResult.map((idx, el) => {
+          const image =
+            $(el).find(".gallerythumb > img").attr("data-src") || "";
+          images.push({
+            imageUrl: image,
+            action: {
+              type: "uri",
+              label: `Page ${idx + 1}`,
+              uri: image,
+            },
+          });
+        })
+      );
+
+      // create a echoing text message
+      const message: Message[] = [
+        {
+          type: "text",
+          text: `Title: ${title}
+  Page: ${pageLength} pages
+  Tags: ${tags.toString()}`,
+        },
+        {
+          type: "template",
+          altText: "image carousel",
+          template: {
+            type: "image_carousel",
+            columns: images,
+          },
+        },
+      ];
+
+      // use reply API
+      return client.replyMessage(event.replyToken, message);
+    }
+    return Promise.resolve(null);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// listen on port
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+
 // app.get("/", async (req, res) => {
 //   const { data } = await axios.get("https://nhentai.net/g/113450/");
 //   const $ = cheerio.load(data);
@@ -70,114 +183,3 @@ const app = express();
 //   // fs.writeFileSync("kek2.html", data);
 //   return res.send("complete");
 // });
-
-app.post("/callback", middleware(middlewareConfig), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
-});
-
-// event handler
-async function handleEvent(event: any) {
-  console.log(event);
-  if (event.type !== "message" || event.message.type !== "text") {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-  const text: string = event.message.text;
-  if (text === "/quit") {
-    if (event.source.type === "room") {
-      const roomId = event.source.roomId;
-      return client.leaveRoom(roomId);
-    }
-    if (event.soure.type === "group") {
-      const groupId = event.source.groupId;
-      return client.leaveGroup(groupId);
-    }
-    return Promise.resolve(null);
-  }
-  //ignore if does not match regex
-  if (!textRegex.test(text)) {
-    return Promise.resolve(null);
-  }
-  //get data
-  console.log("fetch");
-
-  const { data } = await axios.get(`https://nhentai.net/${text}/`);
-  console.log("load");
-  const $ = cheerio.load(data);
-  const title = $("h1[class = title]").text();
-  let imageResult = $("body").find(
-    "#content > #thumbnail-container > .thumbs > .thumb-container"
-  );
-  const pageLength = imageResult.length;
-
-  /* GETTING TAGS*/
-  let tagResult = $('section[id="tags"]').find(
-    `div:contains("Tags") > span[class=tags] > a > span[class=name]`
-  );
-  const tagsLength = tagResult.length;
-  let tags: any[] = [];
-  if (tagsLength > 5) {
-    tagResult = tagResult.slice(0, 5);
-  }
-  await Promise.all(
-    tagResult.map((idx, el) => {
-      tags.push(` ${$(el).text()}`);
-    })
-  );
-  if (tagsLength > 5) {
-    tags.push("...");
-  }
-
-  /* GETTING IMAGES*/
-  let images: TemplateImageColumn[] = [];
-  if (imageResult.length > 5) {
-    imageResult = imageResult.slice(0, 5);
-  }
-  await Promise.all(
-    imageResult.map((idx, el) => {
-      const image = $(el).find(".gallerythumb > img").attr("data-src");
-      images.push({
-        imageUrl: image || "",
-        action: {
-          type: "uri",
-          label: `Page ${idx + 1}`,
-          uri: image || "",
-        },
-      });
-    })
-  );
-
-  // create a echoing text message
-  const message: Message[] = [
-    {
-      type: "text",
-      text: `Title: ${title}
-Page: ${pageLength} pages
-Tags: ${tags.toString()}`,
-    },
-    // { type: "text", text: `` },
-    // { type: "text", text: `` },
-    {
-      type: "template",
-      altText: "image carousel",
-      template: {
-        type: "image_carousel",
-        columns: images,
-      },
-    },
-  ];
-
-  // use reply API
-  return client.replyMessage(event.replyToken, message);
-}
-
-// listen on port
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
