3abf62ce7be78e7587ce0ea9d0558159967164a1
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-04-15 20:46:58 +0700

    add random

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,6 @@ import {
 import express from "express";
 import axios from "axios";
 import dotenv from "dotenv";
-import cheerio from "cheerio";
 import { getData } from "./function";
 dotenv.config();
 
@@ -34,6 +33,12 @@ const textRegex = /^g\/[0-9]{6}$/m;
 
 const app = express();
 // app.use(express.json());
+
+// app.get("/", async (req, res) => {
+//   const { request, data } = await axios.get(`https://nhentai.net/random/`);
+//   console.log(request.path);
+//   return res.send(request.path);
+// });
 
 app.post("/callback", middleware(middlewareConfig), (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
@@ -66,7 +71,7 @@ async function handleEvent(event: WebhookEvent) {
           default:
             return Promise.resolve(null);
         }
-      case (text.match(textRegex) || {}).input:
+      case (text.match(textRegex) || {}).input: {
         //need to check if 404
         const { data } = await axios.get(`https://nhentai.net/${text}/`);
         const { title, tags, page, images } = await getData(data, text);
@@ -92,30 +97,40 @@ Link: https://nhentai.net/${text}/ `,
 
         // use reply API
         return client.replyMessage(event.replyToken, message);
+      }
+
+      case "/random": {
+        const { request, data } = await axios.get(
+          `https://nhentai.net/random/`
+        );
+        const id = request.path.substring(1);
+        const { title, tags, page, images } = await getData(data, id);
+
+        const message: Message[] = [
+          {
+            type: "text",
+            text: `Title: ${title}
+Page: ${page} pages
+Tags: ${tags.toString()}
+Link: https://nhentai.net/${id}/ `,
+          },
+          {
+            type: "template",
+            altText: "image carousel",
+            template: {
+              type: "image_carousel",
+              columns: images,
+            },
+          },
+        ];
+
+        // use reply API
+        return client.replyMessage(event.replyToken, message);
+      }
 
       default:
         return Promise.resolve(null);
     }
-    // /* Quit */
-    // if (text === "/quit") {
-    //   switch (event.source.type) {
-    //     case "room":
-    //       const roomId: string = event.source.roomId;
-    //       return client.leaveRoom(roomId);
-    //     case "group":
-    //       const groupId: string = event.source.groupId;
-    //       return client.leaveGroup(groupId);
-
-    //     default:
-    //       return Promise.resolve(null);
-    //   }
-
-    //   /* If message is g/xxxxxx */
-    // }
-    // if (textRegex.test(text)) {
-
-    // }
-    // return Promise.resolve(null);
   } catch (error) {
     console.log(error);
   }
